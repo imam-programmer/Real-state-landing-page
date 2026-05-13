95d4262ac107fba7d49a184ed0b6b368c9824845
imam-programmer <imamprogrammer09@gmail.com>
2026-05-13 21:39:19 +0600

    full done

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ let Tahapananimation=gsap.timeline({
         scroller:"body",
         start:"top 30%",
         end:"top 0",
-        // scrub:2
+   
         
     }
 })
@@ -56,10 +56,105 @@ Tahapananimation.from("#Tahapan .card",{
     stagger:0.5,  
 })
 
+// =================card part end
 
 
+let Favoritanimation=gsap.timeline({
+    scrollTrigger:{      
+        trigger:"#Favorit",
+        scroller:"body",
+        start:"top 30%",
+    }
+})
+Favoritanimation.from("#Favorit h5",{
+    y:-100,
+    opacity:0,
+    duration:1
+})
+Favoritanimation.from("#Favorit #id",{
+    y:-100,
+    opacity:0,
+    duration:1
+})
+Favoritanimation.from(".Favorit-image-part",{
+    x:-200,
+    opacity:0,
+    duration:1
+},"-=1")
+Favoritanimation.from(".Favorit-text-part",{
+    x:200,
+    opacity:0,
+    duration:1
+},"-=1")
 
+// ==========================favorit part done============
 
+let Berbagaianimation=gsap.timeline({
+      scrollTrigger:{      
+        trigger:"#Berbagai",
+        scroller:"body",
+        start:"top 40%",
+    }
+})
+Berbagaianimation.from("#Berbagai h5",{
+    x:500,
+    opacity:0,
+    duration:1.1
+})
+Berbagaianimation.from("#Berbagai h2",{
+    x:-500,
+    opacity:0,
+    duration:1.1
+})
+Berbagaianimation.from("#Berbagai img,#Berbagai  h4,#Berbagai  p",{
+    x:500,
+    duration:1,
+    opacity:0,
+    stagger:0.5
+})
+// ================
+
+let Penghargaan=gsap.timeline({
+       scrollTrigger:{      
+        trigger:"#Penghargaan",
+        scroller:"body",
+        start:"top 60%",
+    }
+});
+Penghargaan.from("#Penghargaan #P-h2",{
+    y:-100,
+    opacity:0,
+    duration:0.7
+})
+Penghargaan.from("#Penghargaan .card-part .card",{
+    y:100,
+    opacity:0,
+    duration:0.7,
+    stagger:-0.3
+})
+Penghargaan.from("#Penghargaan .img-part h2",{
+    y:100,
+    opacity:0,
+    duration:0.7,
+    stagger:-0.3
+},"-=1")
+Penghargaan.from("#Penghargaan .img-part img",{
+    y:100,
+    opacity:0,
+    duration:0.7,
+    stagger:-0.3
+},)
+Penghargaan.from("footer img",{
+    x:-200,
+    opacity:0,
+    duration:1,
+},)
+Penghargaan.from("footer li",{
+    x:100,
+    opacity:0,
+    duration:0.7,
+    stagger:-0.3
+})
 
 
 
