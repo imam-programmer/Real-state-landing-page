557da039a48c7469ee5997748d89850f231ebf99
imam-programmer <imamprogrammer09@gmail.com>
2026-05-13 18:41:07 +0600

    half animation done

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ let navanimation=gsap.timeline();
 navanimation.from("#nav-img",{
     y:-30,
     opacity:0,
-    duration:1,
+    duration:0.4,
 })
 navanimation.from("nav ul li",{
     opacity:0,
-    // duration:1,
     y:-30,
     stagger:0.3
 })
@@ -23,6 +22,39 @@ navanimation.from("#heru .heru-container .heru-text-part p",{
     x:-100,
 
 },'2')
+navanimation.from("#heru .heru-container .input-container ",{
+    opacity:0,
+    x:-100,
+
+},)
+navanimation.from("#heru-im",{
+    x:100,
+    opacity:0,
+    duration:2
+},"-=2")
+// =================banner done=====================
+
+let Tahapananimation=gsap.timeline({
+    scrollTrigger:{
+        trigger:"#Tahapan",
+        scroller:"body",
+        start:"top 30%",
+        end:"top 0",
+        // scrub:2
+        
+    }
+})
+Tahapananimation.from("#Tahapan h2",{
+    y:100,
+    duration:1,
+    opacity:0,
+})
+Tahapananimation.from("#Tahapan .card",{
+    opacity:0,
+    y:-100,
+    duration:1,
+    stagger:0.5,  
+})
 
 
 
